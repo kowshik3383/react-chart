003b6652dcf250fe4b972bf9b39d6bdebec8d816
kowshik <kowshikvalpireddy@gmail.com>
2024-03-15 09:30:33 +0530

    assdf

--- react-chart/vite.config.js
+++ react-chart/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Adjust the chunk size warning limit
+    chunkSizeWarningLimit: 1000, // Set the limit to 1000 kB (1 MB)
+  },
 })
